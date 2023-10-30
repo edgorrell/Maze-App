142486ecda281067fbc56b2f09f64fa5b5535ad3
Evan Gorrell <edgorrell@stu.naperville203.org>
2023-10-30 12:49:49 -0500

    merged main into maze branch, unit tests work

--- src/Maze.java
+++ src/Maze.java
@@ -1,68 +1,72 @@
 import java.util.*;
 import java.io.*;
 
-public class Maze implements MazeSkeleton{
-    private Square[][]  maze;
+public class Maze{
+    private Square[][] maze = null;
     private Square start, end;
 
     public Maze(){
 
     }
 
-    // there is a better way to do this but it works
-    boolean loadMaze(String fileName) throws FileNotFoundException{
-        File f = new File(fileName);
-        Scanner scan = new Scanner(f);
-        String str = scan.nextLine();  scan.close();
+    boolean loadMaze(String fileName){
+        try{
+            File f = new File(fileName);
+            Scanner scan = new Scanner(f);
+            String str = scan.nextLine();  scan.close();
 
-        int numRows = Integer.parseInt(str.substring(0, str.indexOf(" ")));
-        int numCols = Integer.parseInt(str.substring(str.indexOf(" ")+1));
+            int numRows = Integer.parseInt(str.substring(0, str.indexOf(" ")));
+            int numCols = Integer.parseInt(str.substring(str.indexOf(" ")+1));
 
-        this.maze = new Square[numRows][numCols];
-        scan = new Scanner(f); scan.nextLine();
-        int row = 0, col = 0;
-        while(scan.hasNext()){
-            str = scan.nextLine();
-            for(col = 0; col < numCols; col++){
-                int type = Integer.parseInt(str.substring(2*col,2*col+1));
-                maze[row][col] = new Square(row,col,type);
-                if(type == 2){
-                    this.start = maze[row][col];
+            this.maze = new Square[numRows][numCols];
+            scan = new Scanner(f); scan.nextLine();
+            int row = 0, col = 0;
+            while(scan.hasNext()){
+                str = scan.nextLine();
+                for(col = 0; col < numCols; col++){
+                    int type = Integer.parseInt(str.substring(2*col,2*col+1));
+                    maze[row][col] = new Square(row,col,type);
+                    if(type == 2){
+                        this.start = maze[row][col];
+                    }
+                    if(type == 3){
+                        this.end = maze[row][col];
+                    }
                 }
-                if(type == 3){
-                    this.end = maze[row][col];
-                }
+                row++;
             }
-            row++;
+            return true;
+        } catch(Exception e){
+            return false;
         }
-        return true;
     }
 
-    ArrayList<Square> getNeighbors(Square sq){
+    ArrayList<Square> getNeighbors(int row, int col){
         ArrayList<Square> neighbors = new ArrayList<>();
-
+        Square sq = getSquare(row, col);
+        
         int[] xOffset = {0,1,0,-1};
         int[] yOffset = {1,0,-1,0};
         for(int i = 0; i < 4; i++){
             try{
                 neighbors.add(maze[sq.getRow()+yOffset[i]][sq.getCol()+xOffset[i]]);
-            } catch(Exception e){}
+            } catch(Exception e){
+                continue;
+            }
         }
 
         return neighbors;
     }
 
+    public Square getSquare(int row, int col){
+        return maze[row][col];
+    }
+
     public Square getStart(){
-        if(this.start == null){
-            throw new NullPointerException();
-        }
         return start;
     }
 
     public Square getFinish(){
-        if(this.end == null){
-            throw new NullPointerException();
-        }
         return end;
     }
     
--- src/MazeSkeleton.java
+++
@@ -1,8 +0,0 @@
-public interface MazeSkeleton {
-    public abstract boolean loadMaze(String fname);
-    public abstract ArrayList<Square> getNeighbors(Square sq);
-    public abstract Square getStart();
-    public abstract Square getFinish();
-    public abstract void reset();
-    public abstract String toString();
-}
